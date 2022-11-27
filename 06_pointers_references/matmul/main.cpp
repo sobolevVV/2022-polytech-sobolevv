76a57f4ad491dfe229b63c3b42dbd5ae148eafba
sobolevVV <sobolev.vv@edu.spbstu.ru>
2022-11-27 19:20:23 +0300

    feat(06): add matmul

---
+++ 06_pointers_references/matmul/main.cpp
@@ -0,0 +1,26 @@
+#include "functions.h"
+
+int main()
+{
+    int m_a = 2, n_a = 2;
+    int** a = createArr(m_a, n_a);
+    std::cout << "Fill matrix A:" << std::endl;
+    fillArr(a, m_a, n_a);
+
+    int m_b = 2, n_b = 2;
+    int**b = createArr(m_b, n_b);
+    std::cout << "Fill matrix A:" << std::endl;
+    fillArr(b, m_b, n_b);
+    
+    int m_c = m_a, n_c = n_b;
+    int** c = createArr(m_c, n_c);
+    matmul(a, m_a, n_b, b, m_b, n_b, c);
+
+    std::cout << "A * B = " << std::endl;
+    printArr(c, m_a, n_b);
+
+    deleteArr(a, m_a);
+    deleteArr(b, m_b);
+    deleteArr(c, m_c);
+    return 0;
+}
