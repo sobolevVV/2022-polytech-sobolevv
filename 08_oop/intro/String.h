91a1912c360466b456b3510684c03d2f3fd3cbc0
sobolevVV <sobolev.vv@edu.spbstu.ru>
2022-11-27 19:26:55 +0300

    feat(08): add intro/ and README.md

---
+++ 08_oop/intro/String.h
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstring>
+
+class String
+{
+  private:
+    unsigned int length;
+    char* data;
+    
+  public:
+
+    String()
+    {
+        length = 0;
+        data = new char[1];
+        data[0] = '\0';
+    }
+
+    String(const char* init)
+    {
+        //std::cout << "ctor" << std::endl;
+        length = strlen(init);
+        data = new char[length+1];
+        strcpy(data, init);
+    }
+
+    unsigned int get_length()
+    {
+        return length;
+    }
+
+    const char* c_str()
+    {
+        return data;
+    }
+
+    char operator [] (unsigned int pos)
+    {   
+        return data[pos];
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, String &S)
+    {
+    out << S.c_str();
+    return out;
+    }
+
+    String operator+(String &other) 
+    {
+      auto newString = (char *)malloc(length + other.length);
+      int i, size = length + other.length;
+      int thisLength = this->length, otherLength = other.length;
+
+      for(i = 0; i < thisLength; i++)
+        newString[i] = data[i];
+
+      for(int j = 0; j < otherLength; i++, j++)
+        newString[i] = other[j];
+      
+      newString[thisLength + otherLength] = '\0';
+      return newString;
+    }
+
+    ~String()
+    {
+        delete[] data;
+    }
+};
---
+++ 08_oop/intro/main.cpp
@@ -0,0 +1,12 @@
+
+#include "String.h"
+
+int main() 
+{
+  String a = "I love ";
+  
+  String b = "computer science";
+  String c = a + b;
+  std::cout << c << std::endl;
+  return 0;
+}
